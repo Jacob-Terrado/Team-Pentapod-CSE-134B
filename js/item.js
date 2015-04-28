5780cbd9f3b6bf4681ddf483b21b632153952691
mattvn <matthew.nguyen@me.com>
2015-04-28 16:03:05 -0700

    Added a bit more js for the table
    Need to somehow realign table and add upload image functionality.

--- js/item.js
+++ js/item.js
@@ -1,20 +1,68 @@
 
 
-var gold = {one: 1, two: 2, three : 3, id: gold};
-var silver = {one: 4, two: 5, three : 6, id: silver};
-
-var platinum = {one: 7, two: 8, three : 9, id: platinum};
-
 function metal(value) {
-    
+
     "use strict";
-  
-    if(value == 1) { console.log("gold"); return "Gold %"; }
-    if(value == 2) { console.log("silver");return "Silver %"; }
-    if(value == 3) { console.log("platinum" );return "Platinum %"; }
-    
-    
+
+    if (value == 1) {
+        console.log("gold");
+        return "Gold %";
+    }
+    if (value == 2) {
+        console.log("silver");
+        return "Silver %";
+    }
+    if (value == 3) {
+        console.log("platinum");
+        return "Platinum %";
+    }
+
+
     console.log("Error: Selector tag not returning any value");
+
+};
+
+function metalgu(value) {
+
+    "use strict";
+
+    if (value == 1) {
+        console.log("gold gu");
+        return "Gold g/u";
+    }
+    if (value == 2) {
+        console.log("silver gu");
+        return "Silver g/u";
+    }
+    if (value == 3) {
+        console.log("platinum gu");
+        return "Platinum g/u";
+    }
+
+
+    console.log("Error: G/U Selector tag not returning any value");
+
+};
+
+function metaloz(value) {
+
+    "use strict";
+
+    if (value == 1) {
+        console.log("gold oz");
+        return "Gold ozt/u";
+    }
+    if (value == 2) {
+        console.log("silver oz");
+        return "Silver ozt/u";
+    }
+    if (value == 3) {
+        console.log("platinum oz");
+        return "Platinum ozt/u";
+    }
+
+
+    console.log("Error: OZ Selector tag not returning any value");
 
 };
 
