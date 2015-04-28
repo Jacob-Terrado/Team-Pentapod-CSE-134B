6f51e04e2ec1f17fb961003f4ec04ae9f0757ee5
mvng <matthew.nguyen@me.com>
2015-04-27 23:03:09 -0700

    Added js files for dynamic item switching

---
+++ js/item.js
@@ -0,0 +1,12 @@
+function metalStuff(value) {
+    "use strict";
+    var metalData = '{ "metalVal" : [' +
+    '{ "metal": "Gold", "percent": "0.999", "g": "1.244", "ozt": ".04", "au": ".04", "total": "1199", "id": "1"}' ]}';
+};
+
+/*
+var metalData = '{ "metalVal" : [' +
+    '{ "metal": "Gold",     "percent": "0.999", "g": "1.244", "ozt": ".04", "au": ".04", "total": "1199", "id": "1"},' +
+    '{ "metal": "Silver",   "percent": "0.49", "g": "1.244", "ozt": ".04", "au": ".04", "total": "1199", "id": "2"},' +
+    '{ "metal": "Platinum", "percent": "0.19", "g": "1.244", "ozt": ".04", "au": ".04", "total": "1199", "id": "3"} ]}';
+    */
