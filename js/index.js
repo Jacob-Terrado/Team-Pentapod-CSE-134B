a6b1ef368a8a52d40747d0e90913ec8d427c9954
mattvn <matthew.nguyen@me.com>
2015-05-21 03:33:02 -0700

    New Menu + jschart

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var chartData=
+    {
+	"type": "line",
+    
+	"series": [
+		{ "values": [35, 42, 67, 89, 25, 34, 67, 85 ] },
+		{ "values": [28, 57, 43, 56, 78, 99, 67, 28 ] },
+        { "values": [0, 10, 20, 30, 40, 50, 60, 70 ] }
+	],
+    "plot":{
+      "animation":{
+          "effect":"ANIMATION_EXPAND_BOTTOM",
+          "speed":"1000"
+      },
+  }
+    
+};
+    
+window.onload=function(){
+	zingchart.render({
+		id:'chartDiv',
+		height:450,
+		width:"100%",
+		data: 'chartData',
+        defaultsurl:"resources/sunny.txt"
+	});
+};
